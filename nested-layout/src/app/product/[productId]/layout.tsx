b91fef8bb08aab1a27bd6bc4cf99e11ee249c928
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-03-16 01:37:04 +0530

    nested layouts

---
+++ nested-layout/src/app/product/[productId]/layout.tsx
@@ -0,0 +1,14 @@
+export default function ProductDetailLayout({
+    children,
+}:{
+    children: React.ReactNode
+}){
+    return(
+        <>
+         {children}
+         <h1>feature Products</h1>
+
+         
+        </>
+    )
+}
